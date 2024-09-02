da6c2a415b16de503dbb675ce81416272423a59d
Alireza Balouch <gswape@gmail.com>
2024-09-02 13:06:58 +0200

    using svelte:self for recursive call

--- src/components/Ul.svelte
+++ src/components/Ul.svelte
@@ -1,21 +1,18 @@
 <script>
-import Ul from '../components/Ul.svelte';
-import SingleBox from '../components/SingleBox.svelte';
-export let structure = [];
-
-
+import SingleBox from '../components/SingleBox.svelte'
+export let structure = []
 </script>
+
 <ul>
-{#each structure as node}
-  <li>
-    <div class="box-group">
+  {#each structure as node}
+    <li>
+      <div class="box-group">
         <SingleBox node={node} />
         {#if node.spouse}<SingleBox node={node.spouse} />{/if}
-    </div>
-    {#if node.children && node.children.length > 0}
-    <Ul structure={node.children} />
-    {/if}
-  </li>
+      </div>
+      {#if node.children && node.children.length > 0}
+        <svelte:self structure={node.children} />
+      {/if}
+    </li>
   {/each}
 </ul>
-
