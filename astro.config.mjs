1f29deebfe92ae7ff15c55b2690667c0f77ef982
Alireza Balouch <gswape@gmail.com>
2025-08-16 16:55:54 +0200

    refactor: Update lastUpdateCheck in settings.json, remove postcss.config.mjs, and configure Tailwind CSS in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import {defineConfig} from 'astro/config'
 import svelte from '@astrojs/svelte'
 
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte()],
   vite: {
     server: {
       watch: {
         ignored: ['**/.idea/**/*']
       }
-    }
+    },
+
+    plugins: [tailwindcss()]
   }
 })
